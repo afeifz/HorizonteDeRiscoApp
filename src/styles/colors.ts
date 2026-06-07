2d21db9120e7ae19ba303252941169eea8b921cb
Mohamed <mohamedafifsmaili@gmail.com>
2026-06-06 21:43:48 -0300

    feat colors defined

---
+++ src/styles/colors.ts
@@ -0,0 +1,9 @@
+export const COLORS = {
+  background: "#0D1B2A",
+  card: "#1B263B",
+  primary: "#415A77",
+  text: "#FFFFFF",
+  danger: "#E63946",
+  warning: "#F4A261",
+  success: "#2A9D8F"
+};
