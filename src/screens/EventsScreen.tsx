89f1ced03d722b41d09a36243d479fcbc003c7ee
Mohamed <mohamedafifsmaili@gmail.com>
2026-06-06 21:52:05 -0300

    feat: implementing the screen that's show events

---
+++ src/screens/EventsScreen.tsx
@@ -0,0 +1,44 @@
+import {
+  FlatList,
+  View
+} from "react-native";
+
+import {
+  useEffect,
+  useState
+} from "react";
+
+import { api } from "../services/api";
+import EventCard from "../components/EventCard";
+
+export default function EventsScreen() {
+
+  const [events, setEvents] =
+    useState<any[]>([]);
+
+  useEffect(() => {
+
+    api.get("")
+      .then((response) => {
+
+        setEvents(
+          response.data.events
+        );
+      });
+
+  }, []);
+
+  return (
+
+    <View style={{ flex: 1 }}>
+
+      <FlatList
+        data={events}
+        renderItem={({ item }) =>
+          <EventCard item={item} />
+        }
+      />
+
+    </View>
+  );
+}
